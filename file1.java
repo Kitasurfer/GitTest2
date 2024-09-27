b2c6e3a66e3b054d54a250f45f83c23e933635fb
BogdanF <sovtour@gmail.com>
2024-09-27 11:32:06 +0200

    add Y

--- file1.java
+++ file1.java
@@ -5,6 +5,8 @@ public class file1 {
     public static void main(String[] args) {
 String x = "Hello!!! ";
         System.out.println(x);
+        int y = 10;
+        System.out.println(y);
     }
 
 }
