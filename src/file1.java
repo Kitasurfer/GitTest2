4383a5f340dd7f3ed01341f3c346ad0369f6dc00
BogdanF <sovtour@gmail.com>
2024-09-27 14:28:16 +0200

    test

---
+++ src/file1.java
@@ -0,0 +1,14 @@
+package PACKAGE_NAME;/* Group: 52-1, "AIT Hi-tech team" GMBH 
+Author: Bogdan Fesenko
+Date: 27-09-2024
+*/
+/*
+
+*/
+public class file1 {
+ public static void main(String[] args) {
+
+
+ }
+     
+}
